c15f576a6c588ae385d5a9af73bd3a06acea1a5a
Eric <zhangyl@pencho.com.cn>
2016-02-17 18:04:48 +0800

    add parent view controller

---
+++ MMD/MMD/Expand/Network/EZNetReachabilityManager.h
@@ -0,0 +1,14 @@
+//
+//  EZNetReachabilityManager.h
+//  MMD
+//
+//  Created by pencho on 16/2/17.
+//  Copyright © 2016年 Eric.Co.,Ltd. All rights reserved.
+//
+
+#import "AFNetworkReachabilityManager.h"
+
+@interface EZNetReachabilityManager : AFNetworkReachabilityManager
+
+
+@end
---
+++ MMD/MMD/Main/MMLogViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMLogViewController.h
+//  MMD
+//
+//  Created by pencho on 16/2/16.
+//  Copyright © 2016年 Eric.Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMLogViewController : UIViewController
+
+@end
