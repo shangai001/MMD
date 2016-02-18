a696d62ba9f57969c30cf460660b09432d867adb
Eric <zhangyl@pencho.com.cn>
2016-02-18 18:39:31 +0800

    add slider event

--- MMD/MMD/Main/ApplyViewController.m
+++ MMD/MMD/Main/ApplyViewController.m
@@ -18,6 +18,10 @@
 @property (weak, nonatomic) IBOutlet UIButton *nextButton;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 
+
+@property (assign, nonatomic)int loanCount;
+@property (assign, nonatomic)int refundMonth;
+
 @end
 
 @implementation ApplyViewController
@@ -33,9 +37,52 @@
 }
 - (void)configureSliders{
 
-    [self.loanSlider setThumbImage:[UIImage imageNamed:@"circle_blue_44"] forState:UIControlStateNormal];
-    
-    [self.refundTimeSlider setThumbImage:[UIImage imageNamed:@"Circle_Grey_44"] forState:UIControlStateNormal];
+    {
+        self.loanSlider.minimumValue = 5;
+        self.loanSlider.maximumValue = 50;
+        self.loanSlider.value = 5;
+        
+        
+        [self.loanSlider setThumbImage:[UIImage imageNamed:@"circle_blue_44"] forState:UIControlStateNormal];
+        [self.loanSlider addTarget:self action:@selector(loanCountChanged:) forControlEvents:UIControlEventValueChanged];
+    }
+    {
+        self.refundTimeSlider.minimumValue = 1;
+        self.refundTimeSlider.maximumValue = 6;
+        self.refundTimeSlider.value = 1;
+        
+        [self.refundTimeSlider setThumbImage:[UIImage imageNamed:@"Circle_Grey_44"] forState:UIControlStateNormal];
+        [self.refundTimeSlider addTarget:self action:@selector(refundTimeChanged:) forControlEvents:UIControlEventValueChanged];
+    }
+}
+- (void)loanCountChanged:(UISlider *)slider{
+    float value = slider.value;
+    float newValue = roundf(value);
+    int count = newValue * 100;
+    self.loanCount = count;
+    [self changeLoanCountLabel:count textColor:[UIColor redColor]];
+}
+#pragma mark 此处还需要获取当前颜色值
+- (void)changeLoanCountLabel:(int)count textColor:(UIColor *)color{
+    NSMutableAttributedString *attCountString = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%ld元",(long)count]];
+    NSRange numberRange = NSMakeRange(0, attCountString.length -1);
+    NSDictionary *attrs = @{NSForegroundColorAttributeName : color};
+    [attCountString addAttributes:attrs range:numberRange];
+    self.loanCountLbale.attributedText = attCountString;
+}
+- (void)refundTimeChanged:(UISlider *)slider{
+    float value = slider.value;
+    float newValue = roundf(value);
+    int count = (int)newValue;
+    self.refundMonth = count;
+    [self changeRefundMoth:count textColor:[UIColor orangeColor]];
+}
+- (void)changeRefundMoth:(int)count textColor:(UIColor *)color{
+    NSMutableAttributedString *attCountString = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%ld月",(long)count]];
+    NSRange numberRange = NSMakeRange(0, attCountString.length -1);
+    NSDictionary *attrs = @{NSForegroundColorAttributeName : color};
+    [attCountString addAttributes:attrs range:numberRange];
+    self.loanTimeLabel.attributedText = attCountString;
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
