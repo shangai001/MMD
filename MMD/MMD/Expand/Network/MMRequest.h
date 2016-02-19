52e22f72e1d9cba37b1baf323f7af4c9b85e3f78
Eric <zhangyl@pencho.com.cn>
2016-02-19 15:40:58 +0800

    add network

---
+++ MMD/MMD/Expand/Network/MMRequest.h
@@ -0,0 +1,16 @@
+//
+//  MMRequest.h
+//  MMD
+//
+//  Created by pencho on 16/2/19.
+//  Copyright © 2016年 Eric.Co.,Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+
+@interface MMRequest : NSObject
+
+
+
+@end
