79b37f79e0aa64f36728b7c251217fb8ac03c00b
Eric <zhangyl@pencho.com.cn>
2016-03-01 19:37:42 +0800

    add server define

--- MMD/MMD/AppDelegate.m
+++ MMD/MMD/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <HYBNetworking.h>
+#import "ServerDefine.h"
 
 @interface AppDelegate ()
 
@@ -17,9 +19,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [self configureBaseNetwork];
     return YES;
 }
-
+- (void)configureBaseNetwork{
+    
+    [HYBNetworking updateBaseUrl:kHostURL];
+    [HYBNetworking enableInterfaceDebug:YES];
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
