c95a8b4d52202f41ad2de624892f6acc00b59a57
Eric <zhangyl@pencho.com.cn>
2016-03-01 19:48:51 +0800

    add pod file

---
+++ MMD/MMD/Expand/Const/ServerDefine.h
@@ -0,0 +1,17 @@
+//
+//  NetworkConstHeader.h
+//  MMD
+//
+//  Created by pencho on 16/3/1.
+//  Copyright © 2016年 Eric.Co.,Ltd. All rights reserved.
+//
+#import "ServerOption.h"
+
+#ifndef NetworkConstHeader_h
+#define NetworkConstHeader_h
+
+# if SERVER_OPTION == 1
+#define kHostURL @""
+#endif
+
+#endif /* NetworkConstHeader_h */
---
+++ MMD/MMD/Expand/Const/ServerOption.h
@@ -0,0 +1,15 @@
+//
+//  ServerOption.h
+//  MMD
+//
+//  Created by pencho on 16/3/1.
+//  Copyright © 2016年 Eric.Co.,Ltd. All rights reserved.
+//
+
+#ifndef ServerOption_h
+#define ServerOption_h
+
+
+#define SERVER_OPTION 1
+
+#endif /* ServerOption_h */
