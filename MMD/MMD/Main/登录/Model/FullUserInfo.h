1cc6fdebdf518c18606bf340d6fe8952d718758f
Eric <zhangyl@pencho.com.cn>
2016-04-03 23:49:22 +0800

    add full user info

---
+++ MMD/MMD/Main/登录/Model/FullUserInfo.h
@@ -0,0 +1,13 @@
+//
+//  FullUserInfo.h
+//  MMD
+//
+//  Created by pencho on 16/4/3.
+//  Copyright © 2016年 Eric.Co.,Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FullUserInfo : NSObject
+
+@end
