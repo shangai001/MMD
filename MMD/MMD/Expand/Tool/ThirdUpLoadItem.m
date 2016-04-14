7e3695bf9fc375e4e8280a00aa5368c58440a704
Eric <zhangyl@pencho.com.cn>
2016-04-14 14:54:06 +0800

    替换了图片下载方式

--- MMD/MMD/Expand/Tool/ThirdUpLoadItem.m
+++ MMD/MMD/Expand/Tool/ThirdUpLoadItem.m
@@ -8,6 +8,7 @@
 
 #import "ThirdUpLoadItem.h"
 #import "ZXMemberDetail.h"
+
 #import "HttpRequest.h"
 #import <NSData+YYAdd.h>
 #import "AppUserInfoHelper.h"
@@ -26,48 +27,56 @@ static NSString * const userTakeFaceImageName = @"userTakeImage";
 
 @implementation ThirdUpLoadItem
 
+/*
+//为了能使用 http下载(替换 https--->http,端口替换为80)
 - (NSString *)replaceHttpsString:(NSString *)originalString{
     
     originalString = [originalString stringByReplacingOccurrencesOfString:@"https://" withString:@"http://"];
     originalString = [originalString stringByReplacingOccurrencesOfString:@"9527" withString:@"80"];
     return originalString;
 }
+*/
 - (void)downloadImagesWith:(ZXMemberDetail *)detail{
     
     [SVProgressHUD show];
-    NSLog(@"穿过来的值  %@",detail);
     ZXIDcardModel *idModel = detail.idcardModel;
     __block ThirdUpLoadItem *item = [ThirdUpLoadItem new];
-    
-    
-    NSString *userPhoto = idModel.front_faceImgUrl_user;
-    NSString *frontIDUrl = idModel.front_imgUrl;
-    NSString *backIDUrl = idModel.back_imgUrl;
-    
-    NSString *httpuserPhoto = [self replaceHttpsString:userPhoto];
-    NSString *httpfrontIDUrl = [self replaceHttpsString:frontIDUrl];
-    NSString *httpbackIDUrl = [self replaceHttpsString:backIDUrl];
-    NSLog(@"三个 URL 分别是 %@ %@ %@",httpuserPhoto,httpfrontIDUrl,httpbackIDUrl);
-    
+    /*
+     NSString *userPhoto = idModel.front_faceImgUrl_user;
+     NSString *frontIDUrl = idModel.front_imgUrl;
+     NSString *backIDUrl = idModel.back_imgUrl;
+     NSString *httpuserPhoto = [self replaceHttpsString:userPhoto];
+     NSString *httpfrontIDUrl = [self replaceHttpsString:frontIDUrl];
+     NSString *httpbackIDUrl = [self replaceHttpsString:backIDUrl];
+     NSLog(@"三个 URL 分别是 %@ %@ %@",httpuserPhoto,httpfrontIDUrl,httpbackIDUrl);
+    */
     
     dispatch_group_t downloadGroup = dispatch_group_create();
-    
     //进组
     dispatch_group_enter(downloadGroup);
-    [HttpRequest downloadFile:httpfrontIDUrl baseName:frontIDImageName success:^(id result) {
-        if (result) {
-            NSData *frontData = [NSData dataWithContentsOfFile:result];
-            NSString *frontBase64String = [frontData base64EncodedString];
+    [idModel get_front_img:^(UIImage *image, NSError *error) {
+        if (image) {
+            NSData *imageData = UIImageJPEGRepresentation(image, 1.0);
+            NSString *frontBase64String = [imageData base64EncodedString];
             item.idcardFront = frontBase64String;
-            item.mobileIdCardFront = (NSString *)result;
+            item.mobileIdCardFront = @"";
             //离组
             dispatch_group_leave(downloadGroup);
         }
-    } failure:^(NSError *error) {
-        
     }];
     //进组
     dispatch_group_enter(downloadGroup);
+    [idModel get_back_img:^(UIImage *image, NSError *error) {
+        if (image) {
+            NSData *imageData = UIImageJPEGRepresentation(image, 1.0);
+            NSString *backImageBase64String = [imageData base64EncodedString];
+            item.idcardBack = backImageBase64String;
+            item.mobileIdCardBack = @"";
+            //离组
+            dispatch_group_leave(downloadGroup);
+        }
+    }];
+    /*
     [HttpRequest downloadFile:httpbackIDUrl baseName:backIDImageName success:^(id result) {
         if (result) {
             NSData *backData = [NSData dataWithContentsOfFile:result];
@@ -80,8 +89,20 @@ static NSString * const userTakeFaceImageName = @"userTakeImage";
     } failure:^(NSError *error) {
         
     }];
+     */
     //进组
     dispatch_group_enter(downloadGroup);
+    [idModel get_front_faceImg_user:^(UIImage *image, NSError *error) {
+        if (image) {
+            NSData *userImageData = UIImageJPEGRepresentation(image, 1.0f);
+            NSString *userBase64String = [userImageData base64EncodedString];
+            item.idcardHand = userBase64String;
+            item.mobileIdCardHand = @"";
+            //离组
+            dispatch_group_leave(downloadGroup);
+        }
+    }];
+    /*
     [HttpRequest downloadFile:httpuserPhoto baseName:userTakeFaceImageName success:^(id result) {
         if (result) {
             NSData *userData = [NSData dataWithContentsOfFile:result];
@@ -94,6 +115,7 @@ static NSString * const userTakeFaceImageName = @"userTakeImage";
     } failure:^(NSError *error) {
         
     }];
+     */
     dispatch_group_notify(downloadGroup, dispatch_get_main_queue(), ^{
         // 汇总代码
         [SVProgressHUD dismiss];
