1c5cdf42119c26974c068adce5ac7d420dd4acc8
Eric <zhangyl@pencho.com.cn>
2016-04-19 17:58:01 +0800

    no change

---
+++ MMD/MMD/Main/借款/进度查询/ConstantApplyState.h
@@ -0,0 +1,29 @@
+//
+//  ConstantApplyState.h
+//  MMD
+//
+//  Created by pencho on 16/4/18.
+//  Copyright © 2016年 Eric.Co.,Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSInteger const LOAN_APPLY_CONFIRM;
+extern NSInteger const LOAN_APPLY_CANCLE;
+
+extern NSInteger const LOAN_AUDIT_WAITING;
+extern NSInteger const LOAN_AUDIT_SUCCESS;
+extern NSInteger const LOAN_AUDIT_FAIL;
+extern NSInteger const LAON_AUDIT_CANCLE;
+
+extern NSInteger const LAON_AUDIT_CONFIRM_SUCCESS;
+extern NSInteger const LAON_AUDIT_CONFIRM_FAIL;
+
+extern NSInteger const LAON_FINACE_AUDIT_SUCCESS;
+extern NSInteger const LAON_FINACE_AUDIT_FAIL;
+
+extern NSInteger const LAON_CRANT_FUNDS_SUCCESS;
+
+extern NSInteger const LOAN_FINISH;
+
+
---
+++ MMD/MMD/Main/借款/进度查询/ConstantApplyState.m
@@ -0,0 +1,33 @@
+//
+//  ConstantApplyState.m
+//  MMD
+//
+//  Created by pencho on 16/4/18.
+//  Copyright © 2016年 Eric.Co.,Ltd. All rights reserved.
+//
+
+#import "ConstantApplyState.h"
+
+
+
+
+
+
+NSInteger const LOAN_APPLY_CONFIRM                  = 0;
+NSInteger const LOAN_APPLY_CANCLE                   = 1;
+
+NSInteger const LOAN_AUDIT_WAITING                  = 2;
+NSInteger const LOAN_AUDIT_SUCCESS                  = 3;
+NSInteger const LOAN_AUDIT_FAIL                     = 4;
+NSInteger const LAON_AUDIT_CANCLE                   = 5;
+
+NSInteger const LAON_AUDIT_CONFIRM_SUCCESS          = 6;
+
+NSInteger const LAON_AUDIT_CONFIRM_FAIL             = 7;
+
+NSInteger const LAON_FINACE_AUDIT_SUCCESS           = 8;
+NSInteger const LAON_FINACE_AUDIT_FAIL              = 9;
+
+NSInteger const LAON_CRANT_FUNDS_SUCCESS            = 10;
+
+NSInteger const LOAN_FINISH                         = 11;
