0943144a6b53fd6976bbe386658d9b7ed468f5f6
Eric <zhangyl@pencho.com.cn>
2016-04-26 17:15:38 +0800

    add autolayout

---
+++ MMD/MMD/Main/更多/消息中心/Controller/MessageTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MessageTableViewCell.h
+//  MMD
+//
+//  Created by pencho on 16/4/26.
+//  Copyright © 2016年 Eric.Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageTableViewCell : UITableViewCell
+
+@end
---
+++ MMD/MMD/Main/更多/消息中心/Controller/MessageTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MessageTableViewCell.m
+//  MMD
+//
+//  Created by pencho on 16/4/26.
+//  Copyright © 2016年 Eric.Co.,Ltd. All rights reserved.
+//
+
+#import "MessageTableViewCell.h"
+
+@implementation MessageTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
