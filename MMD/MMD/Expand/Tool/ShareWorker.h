533b81cd26986faf2c6dbf9146d3ac7ca3fb92fd
Eric <zhangyl@pencho.com.cn>
2016-04-27 08:46:28 +0800

    add tool class

---
+++ MMD/MMD/Expand/Tool/ShareWorker.h
@@ -0,0 +1,16 @@
+//
+//  ShareWorker.h
+//  MMD
+//
+//  Created by pencho on 16/4/27.
+//  Copyright © 2016年 Eric.Co.,Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  分享工具类
+ */
+@interface ShareWorker : NSObject
+
+@end
---
+++ MMD/MMD/Expand/Tool/ShareWorker.m
@@ -0,0 +1,13 @@
+//
+//  ShareWorker.m
+//  MMD
+//
+//  Created by pencho on 16/4/27.
+//  Copyright © 2016年 Eric.Co.,Ltd. All rights reserved.
+//
+
+#import "ShareWorker.h"
+
+@implementation ShareWorker
+
+@end
